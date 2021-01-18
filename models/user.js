88a15e801726f447785771651a4b77fbe9023956
Jaydeep1016 <jaydeepvaghela1016jp.jv@gmail.com>
2021-01-18 22:08:51 +0530

    Category Schema

--- models/user.js
+++ models/user.js
@@ -4,43 +4,46 @@ import { v4 as uuidv4 } from "uuid";
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    maxlength: 32,
-    trim: true,
-  },
-  lastname: {
-    type: String,
-    maxlength: 20,
-    trim: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  userinfo: {
-    type: String,
-    trim: true,
-  },
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      maxlength: 20,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    userinfo: {
+      type: String,
+      trim: true,
+    },
 
-  encry_password: {
-    type: String,
-    required: true,
+    encry_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
   },
-  salt: String,
-  role: {
-    type: Number,
-    default: 0,
-  },
-  purchases: {
-    type: Array,
-    default: [],
-  },
-});
+  { timestamps: true }
+);
 
 userSchema
   .virtual("password")
