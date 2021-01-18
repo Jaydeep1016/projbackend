21945e38f7ff6c9f174f7ce0dcf4bb8dfc357e1b
Jaydeep1016 <jaydeepvaghela1016jp.jv@gmail.com>
2021-01-18 12:19:01 +0530

    My first commit

---
+++ models/user.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import crypto from "crypto";
+import { v4 as uuidv4 } from "uuid";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  userinfo: {
+    type: String,
+    trim: true,
+  },
+
+  encry_password: {
+    type: String,
+    required: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+});
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv4();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+//my new gitData
+userSchema.method = {
+  authenticate: function (plainPassword) {
+    return this.securePassword(plainPassword) === this.encry_password;
+  },
+
+  securePassword: function (plainPassword) {
+    if (!plainPassword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
+module.exports = mongoose.model("User", userSchema);
